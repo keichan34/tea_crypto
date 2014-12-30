8d2c58f6d2189abeb79b6a5243d25be22c52dfef
Keitaroh Kobayashi <keita@kkob.us>
2014-12-30 09:27:03 -0500

    Add package.exs for Hex

---
+++ package.exs
@@ -0,0 +1,27 @@
+defmodule TeaCrypto.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :tea_crypto,
+     version: "0.0.1",
+     description: description,
+     package: package,
+     deps: deps]
+  end
+
+  defp deps do
+    []
+  end
+
+  defp description do
+    """
+    A TEA implementation in Erlang.
+    """
+  end
+
+  defp package do
+    [files: ~w(src rebar.config README.md),
+     contributors: ["David Gao", "Keitaroh Kobayashi"],
+     links: %{"GitHub" => "https://github.com/keichan34/tea_crypto"}]
+  end
+end
